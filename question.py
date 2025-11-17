e59373c5b4b12d2005b1c2e8b496d6a4db7d15fd
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 08:47:40 +0000

    auto-commit: 2025m17-084740

--- question.py
+++ question.py
@@ -1,3 +1,13 @@
 class Question:
+
+    {}
+      "difficulty": 100,
+      "question": "What colour is an emerald?",
+      "answers": {
+        "correct": "Green",
+        "wrong": ["Blue", "Red", "Yellow"]
+      }
+    },
+
     def __init__(self, question_dict):
         
