fbaaaa293f9ffeb71e6ea3db39a776e6620df2a8
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:08:40 +0000

    auto-commit: 2025m17-110840

--- game_board.py
+++ game_board.py
@@ -32,12 +32,13 @@ class GameBoard:
 
     def load_questions(self, filename="questions.json"):
         questions_loaded = False
-        while 
-        try:
-            file_to_load = filename
-            content = Path(filename).read_txt()
-            self.questions = json.loads(content)
-        except:
+        while questions_loaded == False:
+            if 
+            try:
+                file_to_load = filename
+                content = Path(filename).read_txt()
+                self.questions = json.loads(content)
+            except:
 
-            print("Could not load questions!")
+                print("Could not load questions!")
 
