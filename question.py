3757c7e950b5749f9a805807c030ae3a9d186938
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:08:14 +0000

    auto-commit: 2025m17-090814

--- question.py
+++ question.py
@@ -19,6 +19,8 @@ class Question:
         self.d = { "text": answers["wrong"][2], "correct": False  } 
     
     def randomise(self):
-        a = 
+        a = self.a
+        b = self.b
+        
         options = [ self.a, self.b, self.c, self.d ]
         options.shuffle()
