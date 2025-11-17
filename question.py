b1758295bea29300bb25d74147ef91da22999b8e
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:28:18 +0000

    auto-commit: 2025m17-092818

--- question.py
+++ question.py
@@ -20,7 +20,9 @@ class Question:
         self.c = { "text": answers["wrong"][1], "correct": False  } 
         self.d = { "text": answers["wrong"][2], "correct": False  } 
     
-    def __str__()
+    def __str__(self):
+        mystr = ""
+        
 
     def randomise(self):
         a = self.a
