0a9c75d573bc43e6a946782048630849539f293e
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:01:53 +0000

    auto-commit: 2025m17-090153

--- tests/test_question_init.py
+++ tests/test_question_init.py
@@ -3,13 +3,12 @@ from question import Question
 def test_question_class():
     # create sample question data
     # matching json storage format
-    question_dict = {
-      "difficulty": 100,
-      "question": "What colour is an emerald?",
-      "answers": {
-        "correct": "Green",
-        "wrong": ["Blue", "Red", "Yellow"]
-      }
+    question_dict = {  "difficulty": 100,
+                        "question": "What colour is an emerald?",
+                        "answers": {
+                          "correct": "Green",
+                          "wrong": ["Blue", "Red", "Yellow"]
+                        }
     }
     # initialise Question object with this data
     my_question = Question(question_dict)
