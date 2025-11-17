dcd43db15aadbe9e375c19f6c45e5752d3339ad3
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:51:21 +0000

    auto-commit: 2025m17-095121

---
+++ check_work.py
@@ -0,0 +1,8 @@
+question_dict = {  "difficulty": 100,
+                        "question": "What colour is an emerald?",
+                        "answers": {
+                                      "correct": "Green",
+                                      "wrong": ["Blue", "Red", "Yellow"] }  } 
+    
+# initialise Question object with this data
+my_question = Question(question_dict)
