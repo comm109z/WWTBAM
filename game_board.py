a5ae6c5c2226afb998a9ea3f4fe790187b81e165
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:45:16 +0000

    auto-commit: 2025m17-114516

--- game_board.py
+++ game_board.py
@@ -60,7 +60,7 @@ class GameBoard:
                 poss_questions.append(item)
         self.question = random.choice(poss_questions)
 
-    def play_round():
+    def play_round(self):
         self.question.display_to_user()
         while self.final_answer == False:
             print("Enter and option A B C or D or L to use a lifeline:")
