af6e4188f1fa8c8848c76c2a9a8711edbcf1d8c6
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 08:51:07 +0000

    auto-commit: 2025m17-085107

--- question.py
+++ question.py
@@ -10,7 +10,7 @@ class Question:
     # },
 
     def __init__(self, question_dict):
-        
+        self.text = 
         answers = question_dict['answers']
         self.a = { "text": answers["correct"], "correct": True  } 
         self.b = { "text": answers["wrong"][0], "correct": False  } 
