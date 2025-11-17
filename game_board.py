66df27803978335e94fd8e656b3fc317e83718ef
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:59:28 +0000

    auto-commit: 2025m17-105928

--- game_board.py
+++ game_board.py
@@ -2,7 +2,7 @@ class GameBoard:
     def __init__(self):
         self.user = None
         self.board = [ 0, 100, 200, 300, 500, 1000, 2000, 4000, 8000,
-                       16000, 32000, 64000, 125000, 250000, 500] # money levels
+                       16000, 32000, 64000, 125000, 250000, 500000, 1000000 ] # money levels
         self.level = 0 # index of round in the board
         self.questions = []
         self.n_lifelines = 3
