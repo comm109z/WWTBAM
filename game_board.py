bfc73e71269a6c2279fb82dcc31934a3c66c8fa0
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:40:44 +0000

    auto-commit: 2025m17-114044

--- game_board.py
+++ game_board.py
@@ -64,6 +64,6 @@ class GameBoard:
 
         self.question.display_to_user()
         while self.final_answer == False:
-            print("Enter and option A - D or 'L' to use a lifeline")
+            print("Enter and option A B C or D or L to use a lifeline")
             user_ans = input("Your answer: ")
 
