5ac01d2102d04b4e9fc9245507619cc59bc5abd7
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:00:55 +0000

    auto-commit: 2025m17-110055

--- check_work.py
+++ check_work.py
@@ -1,21 +1,22 @@
 from question import Question
+from game_board iumport 
 
-question_dict = {  "difficulty": 100,
-                        "question": "What colour is an emerald?",
-                        "answers": {
-                                      "correct": "Green",
-                                      "wrong": ["Blue", "Red", "Yellow"] }  } 
+# question_dict = {  "difficulty": 100,
+#                         "question": "What colour is an emerald?",
+#                         "answers": {
+#                                       "correct": "Green",
+#                                       "wrong": ["Blue", "Red", "Yellow"] }  } 
     
-# initialise Question object with this data
-my_question = Question(question_dict)
-print(my_question)
-print(f"a is ... {my_question.check_ans('a')}")
-print(f"b is ... {my_question.check_ans('b')}")
-print(f"b is ... {my_question.check_ans('c')}")
-print(f"b is ... {my_question.check_ans('d')}")
-my_question.fiftyfifty()
-print(my_question)
-print(f"a is ... {my_question.check_ans('a')}")
-print(f"b is ... {my_question.check_ans('b')}")
-print(f"b is ... {my_question.check_ans('c')}")
-print(f"b is ... {my_question.check_ans('d')}")
+# # initialise Question object with this data
+# my_question = Question(question_dict)
+# print(my_question)
+# print(f"a is ... {my_question.check_ans('a')}")
+# print(f"b is ... {my_question.check_ans('b')}")
+# print(f"b is ... {my_question.check_ans('c')}")
+# print(f"b is ... {my_question.check_ans('d')}")
+# my_question.fiftyfifty()
+# print(my_question)
+# print(f"a is ... {my_question.check_ans('a')}")
+# print(f"b is ... {my_question.check_ans('b')}")
+# print(f"b is ... {my_question.check_ans('c')}")
+# print(f"b is ... {my_question.check_ans('d')}")
