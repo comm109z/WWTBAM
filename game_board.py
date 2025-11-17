6e9cb2f9d34c8efa03e961108ab5c591cd2dbd16
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:48:28 +0000

    auto-commit: 2025m17-114828

--- game_board.py
+++ game_board.py
@@ -66,7 +66,7 @@ class GameBoard:
         while self.final_answer == False:
             print("Enter and option A B C or D or L to use a lifeline:")
             user_ans = input("> ").lower()
-            if user_ans == "l" and self.n_lifeline > 0:
+            if user_ans == "l" and self.n_lifelines > 0:
                 self.question.fiftyfifty()
             elif self.question.check_ans(user_ans) != "invalid":
                 print("Final answer?... (Y/N)")
