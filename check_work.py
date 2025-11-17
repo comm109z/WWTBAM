6a214d44dd35948b3132c3deaeb42bd83b6aa676
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:15:23 +0000

    auto-commit: 2025m17-101523

--- check_work.py
+++ check_work.py
@@ -8,7 +8,7 @@ question_dict = {  "difficulty": 100,
     
 # initialise Question object with this data
 my_question = Question(question_dict)
-
+print(f"my_question
 my_question.randomise()
 
 my_question.fiftyfifty()
