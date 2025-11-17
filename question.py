298c94c92af8b7ce859f6b390250f4c3aae7eaa4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:29:34 +0000

    auto-commit: 2025m17-092934

--- question.py
+++ question.py
@@ -23,10 +23,11 @@ class Question:
     def __str__(self):
         mystr = ""
         mystr += f"Q. {self.text}\n"
-        mystr += f"A. {self.a}\n"
-        mystr += f"B. {self.a}\n"
-        mystr += f"D. {self.a}\n"
-        mystr += f"D. {self.a}\n"
+        mystr += f"A. {self.a (self.) }\n"
+        mystr += f"B. {self.b}\n"
+        mystr += f"D. {self.c}\n"
+        mystr += f"D. {self.d}\n"
+
 
     def randomise(self):
         a = self.a
