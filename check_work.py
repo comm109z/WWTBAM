bfe75b34f960674eed74b8f483354283e9da6073
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:16:39 +0000

    auto-commit: 2025m17-101639

--- check_work.py
+++ check_work.py
@@ -9,13 +9,13 @@ question_dict = {  "difficulty": 100,
 # initialise Question object with this data
 my_question = Question(question_dict)
 print(my_question)
-print(f"a is ... {my_question.check_ans('a')"}
-print(f"b is ... {my_question.check_ans('b')}
-print(f"b is ... {my_question.check_ans('c')}
-print(f"b is ... {my_question.check_ans('d')}
+print(f"a is ... {my_question.check_ans('a')}"
+print(f"b is ... {my_question.check_ans('b')}"
+print(f"b is ... {my_question.check_ans('c')}"
+print(f"b is ... {my_question.check_ans('d')}"
 my_question.fiftyfifty()
 print(my_question)
-print(f"a is ... {my_question.check_ans('a')}
-print(f"b is ... {my_question.check_ans('b')}
-print(f"b is ... {my_question.check_ans('c')}
+print(f"a is ... {my_question.check_ans('a')}"
+print(f"b is ... {my_question.check_ans('b')}"
+print(f"b is ... {my_question.check_ans('c')}"
 print(f"b is ... {my_question.check_ans('d')}
