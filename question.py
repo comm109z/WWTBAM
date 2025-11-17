cabde0624a9f330e201d6e9f36df4a01fdad376c
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 08:50:12 +0000

    auto-commit: 2025m17-085012

--- question.py
+++ question.py
@@ -12,4 +12,7 @@ class Question:
     def __init__(self, question_dict):
         answers = question_dict['answers']
         self.a = { "text": answers["correct"], "correct": True  } 
+        self.b = { "text": answers["correct"], "correct": True  } 
+        self.c = { "text": answers["correct"], "correct": True  } 
+        self.a = { "text": answers["correct"], "correct": True  } 
         
