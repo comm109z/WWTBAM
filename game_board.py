fa1ab069d49257997386526576ff22326321fc24
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 12:10:10 +0000

    auto-commit: 2025m17-121010

--- game_board.py
+++ game_board.py
@@ -89,6 +89,7 @@ class GameBoard:
             # If the user entered a valid answer choice (A, B, C, or D)
             elif self.question.check_ans(user_ans) != "invalid":
                 print(f"You are playing for £{self.question.difficulty}")
+                print(f"")
                 print("Final answer?... (Y/N)")
                 final_ans = input("> ")
 
