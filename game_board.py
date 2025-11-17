633124cb16b6bfb59082fae7474092a0bc2832e7
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:45:27 +0000

    auto-commit: 2025m17-114527

--- game_board.py
+++ game_board.py
@@ -70,7 +70,7 @@ class GameBoard:
             elif self.question.check_ans(user_ans) != "invalid":
                 print("Final answer?... (Y/N)")
                 final_ans = input("> ")
-                if final_ans..lower() == "y":
+                if final_ans.lower() == "y":
                     self.final_answer == True
         print(f"You selected: {user_ans.upper()}")
         if self.question.check_ans(user_ans) == "correct":
