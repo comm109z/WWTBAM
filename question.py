350b3417fff30de1662a1d8e6cfc966485a39ede
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:27:32 +0000

    auto-commit: 2025m17-102732

--- question.py
+++ question.py
@@ -70,10 +70,7 @@ class Question:
 
         # blank remaining 2 / 3
         for item in wrong_ans:
-            if item == "a": self.a["text"] = ""
-            elif item == "b": self.b["text"] = ""
-            elif item == "c": self.c["text"] = ""
-            elif item == "d": self.d["text"] = ""
+            
 
         self.fiftyfifty_used = True
         return True
