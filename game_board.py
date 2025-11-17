52cf8f424a33976d2fc14bfea31c62a1f0392dcc
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:32:47 +0000

    auto-commit: 2025m17-113247

--- game_board.py
+++ game_board.py
@@ -58,4 +58,4 @@ class GameBoard:
         for item in self.questions:
             if item.difficulty == money_amount:
                 poss_questions.append(item)
-        self.question = random.choice(poss_questions)
+        self.question = poss_questions(random.ran)
