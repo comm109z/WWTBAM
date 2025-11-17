3c24f905982f60eda4ea2020d44d56a592346916
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 12:12:32 +0000

    auto-commit: 2025m17-121232

--- game_board.py
+++ game_board.py
@@ -125,9 +125,9 @@ class GameBoard:
             if result == True:
                 # Player answered correctly → move up one level
                 self.level += 1
+                # r
                 if self.level in self.safe_levels:
-                    self.winnings = 
-
+                    self.winnings = self.board[self.level]
 
                 # If the player is past the final level, end the game
                 if self.level >= len(self.board):
