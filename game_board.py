06e8ca0f338c3cd110769d7c917cf4e5739ed3ba
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 12:14:52 +0000

    auto-commit: 2025m17-121452

--- game_board.py
+++ game_board.py
@@ -135,7 +135,7 @@ class GameBoard:
                 if self.level >= len(self.board):
                     ended = True
 
-
+            
 
             else:
                 # Player answered incorrectly → game ends immediately
