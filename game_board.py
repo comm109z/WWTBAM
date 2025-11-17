c047d84efedbad464804b2cbd8ffa77060133872
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 12:12:00 +0000

    auto-commit: 2025m17-121200

--- game_board.py
+++ game_board.py
@@ -8,7 +8,7 @@ class GameBoard:
         self.user = None
         self.board = [ 0, 100, 200, 300, 500, 1000, 2000, 4000, 8000,
                        16000, 32000, 64000, 125000, 250000, 500000, 1000000 ] # money levels
-        self.winnings = [ 5, 11 ] 
+        self.safe_levels = [ 5, 11 ] 
         self.level = 0 # index of round in the board
         self.questions = []
         self.n_lifelines = 3
@@ -125,7 +125,7 @@ class GameBoard:
             if result == True:
                 # Player answered correctly → move up one level
                 self.level += 1
-
+            
                 # If the player is past the final level, end the game
                 if self.level >= len(self.board):
                     ended = True
