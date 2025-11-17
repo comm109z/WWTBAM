aa3ca3d09f47952651dae5f79f8bff9ee2057d0a
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:54:56 +0000

    auto-commit: 2025m17-105456

--- game_board.py
+++ game_board.py
@@ -4,4 +4,5 @@ class GameBoard:
         self.board = [] # money levels
         self.level = 0 # index of round in the board
         self.questions = []
-        self.n
+        self.n_lifelines = 3
+        self.
