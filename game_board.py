41ae901f595690bf7e12a64c887ba05d8b4f8529
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 11:00:23 +0000

    auto-commit: 2025m17-110023

--- game_board.py
+++ game_board.py
@@ -19,4 +19,4 @@ class GameBoard:
             row_str += f"£{self.board[idx]}\n"
             mystr += row_str
         mystr += f"Lifelines: {self.n_lifelines}\n"
-        mystr += f"Answer locked in: {self.n_lifelines}\n"
+        mystr += f"Answer locked in: {self.final_answer}\n"
