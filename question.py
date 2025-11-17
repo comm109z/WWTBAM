5f71b7cb6f45356de2f9acc60fb0ccc2f918287f
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 10:14:17 +0000

    auto-commit: 2025m17-101417

--- question.py
+++ question.py
@@ -93,19 +93,19 @@ class Question:
             # otherwise must be wrong
             else:
                 return "wrong"
-        if user_ans == "a":
+        if user_ans == "c":
             # check answer not eliminated
-            if self.a['text'] == "":
+            if self.c['text'] == "":
                 return "invalid"
             # check if correct
-            elif self.a["correct"]:
+            elif self.c["correct"]:
                 return "correct"
             # otherwise must be wrong
             else:
                 return "wrong"
-        if user_ans == "a":
+        if user_ans == "d":
             # check answer not eliminated
-            if self.a['text'] == "":
+            if self.d['text'] == "":
                 return "invalid"
             # check if correct
             elif self.a["correct"]:
