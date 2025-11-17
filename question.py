391a873e0b98e3861894655a3cc3981d8c6e1849
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-17 09:31:10 +0000

    auto-commit: 2025m17-093110

--- question.py
+++ question.py
@@ -23,7 +23,7 @@ class Question:
     def __str__(self):
         mystr = ""
         mystr += f"Q. {self.text}\n"
-        mystr += f"A. {self.a ({self.a[correct}) }\n"
+        mystr += f"A. {self.a['text'] ({self.a['correct']}) }\n"
         mystr += f"B. {self.b ({self.correct})}\n"
         mystr += f"D. {self.c ({self.correct})}\n"
         mystr += f"D. {self.d ({self.correct})}\n"
